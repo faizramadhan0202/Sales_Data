8738e9bb9d04b78c036a493f4e7a6e9d1439f7f0
faizramadhan0202 <faizramadhan0202@gmail.com>
2022-11-28 12:29:05 +0700

    Update Data SQL

--- Sales_Data.sql
+++ Sales_Data.sql
@@ -43,3 +43,78 @@ from PortpolioProject02..sales_data_sample
 where YEAR_ID = 2004 AND MONTH_ID = 11
 group by MONTH_ID, PRODUCTLINE
 order by 3
+
+--Who is our best customer (this could be best answered with RFM)
+IF EXISTS(select * from PortpolioProject02..sales_data_sample) 
+BEGIN
+   DROP TABLE #rfm;
+END;
+
+--CTE
+;with rfm as 
+(
+	select 
+		CUSTOMERNAME, 
+		sum(sales) MonetaryValue,
+		avg(sales) AvgMonetaryValue,
+		count(ORDERNUMBER) Frequency,
+		max(ORDERDATE) last_order_date,
+		(select max(ORDERDATE) from PortpolioProject02..sales_data_sample) max_order_date,
+		DATEDIFF(DD, max(ORDERDATE), (select max(ORDERDATE) from PortpolioProject02..sales_data_sample)) Recency
+	from PortpolioProject02..sales_data_sample
+	group by CUSTOMERNAME
+),
+rfm_calc as
+(
+	select r.*,
+		NTILE(4) OVER (order by Recency desc) rfm_recency,
+		NTILE(4) OVER (order by Frequency) rfm_frequency,
+		NTILE(4) OVER (order by MonetaryValue) rfm_monetary
+	from rfm r
+	--order by 4 desc
+)
+select 
+	c.*, rfm_recency + rfm_frequency + rfm_monetary as rfm_cell,
+	cast(rfm_recency as varchar) + cast(rfm_frequency as varchar) + cast(rfm_monetary as varchar) as rfm_cell_string
+into #rfm
+from rfm_calc c
+
+--CASE WHEN
+select CUSTOMERNAME, rfm_recency, rfm_frequency, rfm_monetary,
+	case 
+		when rfm_cell_string in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) then 'lost_customers'  --lost customers
+		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+		when rfm_cell_string in (311, 411, 331) then 'new customers'
+		when rfm_cell_string in (222, 223, 233, 322) then 'potential churners'
+		when rfm_cell_string in (323, 333,321, 422, 332, 432) then 'active' --(Customers who buy often & recently, but at low price points)
+		when rfm_cell_string in (433, 434, 443, 444) then 'loyal'
+	end rfm_segment
+
+from #rfm
+
+--What products are most often sold together? 
+--select *
+--from PortPolioProject02..sales_data_sample
+--where ORDERNUMBER = 10411
+
+select distinct ORDERNUMBER, Stuff(
+	(select ',' + PRODUCTCODE
+	from PortpolioProject02..sales_data_sample p
+	where ORDERNUMBER IN
+		(
+			select ORDERNUMBER
+			from (
+					select ORDERNUMBER, count(*) rn
+					from PortpolioProject02..sales_data_sample
+					where status = 'Shipped'
+					group by ORDERNUMBER
+				)rn
+			where rn = 2
+		)
+		and p.ORDERNUMBER = s.ORDERNUMBER
+		for xml path (''))
+
+		, 1, 1, '') ProductCodes
+
+	from PortpolioProject02..sales_data_sample s
+	order by 2 desc
